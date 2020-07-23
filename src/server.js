fe03e1244d963350a48969ddab4b1c7de3231a53
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-23 11:42:31 -0300

    refator: referenciando rota produtos

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const products = require('./routes/products')
+const connection = require('./database/connection')
 
+app.use('/products', products)
 
-
-app.listen(5000,() =>{
+app.listen(process.env.PORT,() => {
     console.log("Server is running")
 })
