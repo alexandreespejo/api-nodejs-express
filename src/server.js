a36ae53e20276d0503b4d603412089b5d0a15514
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-24 17:57:19 -0300

    feat: referenciando rota user

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const comments = require('./routes/comments')
+const users = require('./routes/users')
 
 app.use(express.json())
 
 app.use('/comments', comments)
+
+app.use('/users', users)
+
 
 app.listen(process.env.PORT,() => {
     console.log("Server is running")
