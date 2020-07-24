28c49cf10854939155b401a3e5a34dad401877a3
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-24 14:22:50 -0300

    feat: metodo update

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -7,12 +7,24 @@ class ProductsController{
             return res.status(200).json({products})
         })
     }
+
     async create(req,res){
         const {name,price} = req.body
         await Products.create({name,price}).then(products => {
             return res.status(201).json({Response:"Product Created"})
         }).catch((err) => {
             return res.status(404).json({Response:"Product not Created ",err})
+        })
+    }
+
+    async update(req,res){
+        const {id} = req.params
+        const {name,price} = req.body
+
+        await Products.update({name,price},{where:{id}}).then(() => {
+            return res.status(201).json({Response:"Product Updated"})
+        }).catch((err) =>{
+            return res.status(404).json({Response:"Product not Updated ",err})
         })
     }
 
