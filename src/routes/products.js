32056736fe2e97a86340808b2b2e95c2b64f8af6
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-24 14:23:20 -0300

    feat: rota update passando id do produto

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const ProductsController = require('../controllers/Products')
 
 router.get('/list', ProductsController.list)
+
 router.post('/create', ProductsController.create)
+
+router.put('/update/:id', ProductsController.update)
+
 
 module.exports = router
