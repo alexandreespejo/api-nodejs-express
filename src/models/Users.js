3e002acb95123c344885d77b8862384d30f4abfb
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-24 17:58:10 -0300

    add: model users

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,1 +1,17 @@
 const {DataTypes} = require('sequelize')
+const connection = require('../database/connection')
+
+const Users = connection.define('users', {
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    pass:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
+})
+
+Users.sync()
+
+module.exports = Users
