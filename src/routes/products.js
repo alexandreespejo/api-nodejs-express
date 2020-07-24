3570d473ac6aafe813509907b94db6381388a65f
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-24 15:05:35 -0300

    feat: rota delete

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = express.Router()
 const ProductsController = require('../controllers/Products')
 
-router.get('/list', ProductsController.list)
+router.get('/list/:id?', ProductsController.list)
 
 router.post('/create', ProductsController.create)
 
 router.put('/update/:id', ProductsController.update)
+
+router.delete('/delete/:id', ProductsController.delete)
+
 
 
 module.exports = router
