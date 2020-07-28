5a80590e6b1fccd044747f6bf2a1b769c2100561
alexandreespejo <alexandreespejo98@gmail.com>
2020-07-28 14:39:07 -0300

    feat: utilizando alter

--- src/models/start.js
+++ src/models/start.js
@@ -2,10 +2,10 @@ const Users = require('./Users')
 const Comments = require('./Comments')
 
 Users.hasMany(Comments)
-Comments.belongsTo(Users)
+Comments.belongsTo(Users) 
 
-Users.sync().then(() => {
-    Comments.sync()
+Users.sync({alter:true}).then(() => {
+    Comments.sync({alter:true})
 })
 
 
