1d80296a0991d791b6b1ebb4ea7260bb11c154c6
alexandreespejo <alexandreespejo98@gmail.com>
2020-08-17 20:17:38 -0300

    add: rota das reserves

---
+++ src/routes/reserves.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.router()
+const ReservesController = require('../controllers/Reserves')
+
+router.get('/reserves', ReservesController.index)
+
+router.post('/reserves', ReservesController.create)
+
+router.put('/reserves', ReservesController.update)
+
+router.delete('/reserves', ReservesController.destroy)
+
+
+
+module.exports = router
